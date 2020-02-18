6b3d546b463a8585c884163a6ea6d2a81b0c36e1
Alina Ilina <alinailina.random@gmail.com>
2020-02-18 12:36:37 +0200

    Edited EventList.jsx

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,27 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import EventListItem from './EventListItem';
-import './styles/EventListStyle.scss';
+import EventListItem from "./EventListItem";
+import "./styles/EventListStyle.scss";
 
 export default function EventList(props) {
-    const [list, setList] = useState([1, 2, 3]);
-
-
-
-
-
-    return (
-
-        <div className="event-list">
-            <h1>Our Events</h1>
-
-            {list.map(item => (
-                <div key={item}>{
-                    <EventListItem
-                        count={item}
-                    />
-                }</div>))}
-
-        </div>
-    )
+  return (
+    <div className="event-list">
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </div>
+  );
 }
--- src/components/EventListItem.jsx
+++ src/components/EventListItem.jsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './styles/EventListItemStyle.scss';
+import React, { useState, useEffect } from "react";
+import "./styles/mainStyles.scss";
+import "./styles/EventListItemStyle.scss";
 
-
+// Placeholder image:
+//<img src={require(".././assets/placeholder-image.jpg")} />
 
 export default function EventListItem(props) {
-    const [count, setCount] = useState();
-
-
-    return (
-        <div className="event-list-item">
-            <article>
-                <div className="image">
-                    <img src={require('.././assets/placeholder-image.jpg')} />
-                </div>
-                <div className="description">
-                    <h2>Activity {count}</h2>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-                aspernatur! Ad recusandae consequatur quia itaque similique. Iure
-                iusto ipsam optio tenetur nemo, odio doloribus, est tempora,  </p>
-                    <button>Register</button>
-                </div>
-            </article>
-            <hr />
-        </div>
-    )
+  return (
+    <div className="event-list-item">
+      <div className="image">
+        <p>
+          <span>Feb</span>
+          12
+        </p>
+      </div>
+      <div className="text">
+        <h3>Activity title</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+          aspernatur! Ad recusandae consequatur quia itaque similique.
+        </p>
+        <button>Register</button>
+      </div>
+    </div>
+  );
 }
