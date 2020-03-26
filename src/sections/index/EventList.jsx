3235aa6c77b0c4a62db7be944998e0edde8d13ba
OlSavMe <olga_savka.os@mail.ru>
2020-03-27 01:32:07 +0200

    merge

--- src/sections/index/EventList.jsx
+++ src/sections/index/EventList.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from "react";
 import "./stylesheets/EventListStyles.scss";
+<<<<<<< Updated upstream
 import css from "./Events.module.scss";
 import Axios from 'axios';
 
 export default function EventList() {
+=======
+import Axios from 'axios';
+
+export default function EventList() {
+  const [startDate, setStartDate] = React.useState([]);
+>>>>>>> Stashed changes
   const [events, setEvents] = React.useState([]);
   const URL =
     "https://theshortcut.org/wp-json/wp/v2/events/?per_page=100";
@@ -78,17 +85,27 @@ export default function EventList() {
 
 
   return (
+<<<<<<< Updated upstream
     <div className={css.events}>
       <h3 className={css.heading}>UPCOMING EVENTS</h3>
       <div className={css.container}>
         {events.map((event, i) => (
           <div key={i} className={css.event}>
             <div className={css.event}>
+=======
+    <div className="event-list">
+      <h3>UPCOMING EVENTS</h3>
+      <div className='eventcontainer'>
+        {events.map((event, i) => (
+          <div key={i} className="event-list-item">
+            <div className="image">
+>>>>>>> Stashed changes
               <img
                 src={event.acf.image.url}
                 alt="event"
               />
             </div>
+<<<<<<< Updated upstream
             <div className={css.info}>
               <p className={css.date}>{formatDate(event.acf.date_start)} {formatTime(event.acf.time_start)} {formatTime(event.acf.time_end)} </p>
               <h4 className={css.title}>{event.acf.title}</h4>
@@ -103,6 +120,19 @@ export default function EventList() {
           </div>))}
       </div>
       <a className={css.link} href="/#">See all events</a>
+=======
+            <div className="text">
+              <p className="date">{formatDate(event.acf.date_start)} {formatTime(event.acf.time_start)} {formatTime(event.acf.time_end)} </p>
+              <h3>{event.acf.title}</h3>
+              <p>{event.acf.description}</p>
+              <p className="address">
+                {event.acf.location}
+              </p>
+            </div>
+          </div>))}
+      </div>
+      <a href="/#">See all events</a>
+>>>>>>> Stashed changes
     </div >
   );
 }
