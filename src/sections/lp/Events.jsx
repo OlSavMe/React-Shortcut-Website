7ccac0aa0bfc1677cdd42a119621f382c64b1b14
Alina Ilina <alinailina.random@gmail.com>
2020-03-30 15:08:41 +0300

    Events flex fix

--- src/sections/lp/Events.jsx
+++ src/sections/lp/Events.jsx
@@ -1,26 +1,20 @@
 import React, { useEffect } from "react";
 import css from "./Events.module.scss";
-import Axios from 'axios';
+import Axios from "axios";
 
 export default function Events() {
   const [events, setEvents] = React.useState([]);
-  const URL =
-    "https://theshortcut.org/wp-json/wp/v2/events/?per_page=6";
-
+  const URL = "https://theshortcut.org/wp-json/wp/v2/events/?per_page=6";
 
   useEffect(() => {
     getEvents();
-
   }, []);
-
 
   const getEvents = async () => {
     await Axios.get(URL)
       .then(response => response.data)
       .then(data => {
         setEvents(data);
-
-
       });
   };
 
@@ -31,78 +25,74 @@ export default function Events() {
   //   return `${tomorrow.getFullYear()}/${tomorrow.getMonth() + 1}/${tomorrow.getDate()}`;
   // }
 
-
-  const formatDate = (e) => {
-
+  const formatDate = e => {
     const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
-    ]
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
 
-    const days = [
-      'Sun',
-      'Mon',
-      'Tue',
-      'Wed',
-      'Thu',
-      'Fri',
-      'Sat'
-    ]
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     const dateParts = e.split("/");
     const d = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
     console.log(d);
-    const date = d.getDate()
+    const date = d.getDate();
     const dayName = days[d.getDay()];
-    const monthName = months[d.getMonth()]
+    const monthName = months[d.getMonth()];
     const formatted = `${dayName}, ${date} ${monthName}`;
     console.log(formatted);
-    return (formatted);
-
+    return formatted;
   };
 
-  const formatTime = (e) => {
+  const formatTime = e => {
     const t = e.slice(0, -3);
-    return (t);
-  }
-
+    return t;
+  };
 
   return (
     <div className={css.events}>
       <h3 className={css.heading}>Upcoming events</h3>
+
       <div className={css.container}>
-        <div className={css.eventlist}>
-          {events.map((event, i) => (
-            <div key={i} className={css.event}>
-              <img
-                src={event.acf.image.url}
-                alt="event"
-              />
-              <div className={css.info}>
-                <p className={css.date}>{formatDate(event.acf.date_start)} {formatTime(event.acf.time_start)} {formatTime(event.acf.time_end)} </p>
+        {events.map((event, i) => (
+          <div key={i} className={css.event}>
+            <div className={css.visual}>
+              <img src={event.acf.image.url} alt="event" />
+            </div>
+            <div className={css.info}>
+              <div className={css.text}>
+                <p className={css.date}>
+                  {formatDate(event.acf.date_start)}{" "}
+                  {formatTime(event.acf.time_start)}{" "}
+                  {formatTime(event.acf.time_end)}{" "}
+                </p>
                 <h4 className={css.title}>{event.acf.title}</h4>
                 <p className={css.description}>{event.acf.description}</p>
-                <p className={css.address}>
-                  {event.acf.location}
-                </p>
+                <p className={css.address}>{event.acf.location}</p>
+              </div>
+              <div className={css.btn}>
                 <a className={css.link} href={event.acf.register}>
-                  Learn more </a>
+                  Learn more{" "}
+                </a>
               </div>
             </div>
-          ))}
-        </div>
-        <a className={css.link} href="/#">See all events</a>
+          </div>
+        ))}
       </div>
-    </div >
+
+      <a className={css.link} href="/#">
+        See all events
+      </a>
+    </div>
   );
 }
