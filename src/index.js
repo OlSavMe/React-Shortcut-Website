a4814b3b31e02c67d4ac52379bfff66aa3a75ec5
Alina Ilina <alinailina.random@gmail.com>
2020-03-30 11:41:44 +0300

    Fixed nav, footer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
