54cf60dd17f8e61305a567f6e918d2182db31e9f
Alina Ilina <alinailina.random@gmail.com>
2020-05-08 12:25:10 +0300

    Changed hero text layout

--- src/sections/home/1_hero/Hero.jsx
+++ src/sections/home/1_hero/Hero.jsx
@@ -9,9 +9,13 @@ const Hero = () => {
       <div>
         <h1>The Shortcut</h1>
         <h2>Accelerating your entry into the startup world</h2>
-        <div>
-          <button>Join us</button>
-          <button>Work with us</button>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aut
+          magnam, facilis, quos, unde ea magni in est eum porro blanditiis enim.
+        </p>
+        <div className={css.buttons}>
+          <a href="/">Join us</a>
+          <a href="/">Work with us</a>
         </div>
       </div>
     </div>
